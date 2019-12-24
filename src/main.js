69954fdbfddf1e02e4e93f20f13a5e22f63121a7
Marjan Mitev <marjan.mitev@massive.co>
2019-12-24 09:38:34 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export const Hello = () => {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Hello world!</div>
+    </>
+  )
+}
